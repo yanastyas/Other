c7b2fafa4128c9a81c9bbd7eb133710e9f8b26ed
Filipp Voronov <fivoronov@gmail.com>
2022-08-20 22:24:01 +0300

    Add bonus

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
     public static int[] prices = { 50, 135, 65, 53 };
+    public static int MIN_COST_FOR_BONUS = 1000;
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
@@ -35,9 +36,12 @@ public class Main {
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
         for (int i = 0; i < products.length; i++) {
+            sum += prices[i] * counts[i];
+        }
+        boolean doBonus = sum >= MIN_COST_FOR_BONUS;
+        for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
-                System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
-                sum += prices[i] * counts[i];
+                System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
             }
         }
         System.out.println("Итого: " + sum + " руб.");
