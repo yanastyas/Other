32d22e9ea3ce742e868ead31550e90da574d0429
Filipp Voronov <fivoronov@gmail.com>
2022-08-20 22:02:50 +0300

    Add sale 3 as 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
     public static int[] prices = { 50, 135, 65, 53 };
 
+    // В стоимости этих товаров каждые три товара должны стоить как два:
+    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
         System.out.println("Наш ассортимент:");
@@ -36,10 +39,22 @@ public class Main {
         int sum = 0;
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
-                System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                boolean isOnSale = false;
+                for (String saleProduct : productsOnSale) {
+                    if (products[i].equals(saleProduct)) {
+                        isOnSale = true;
+                    }
+                }
+
+                if (isOnSale) {
+                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
+                } else {
+                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                }
                 sum += prices[i] * counts[i];
             }
         }
         System.out.println("Итого: " + sum + " руб.");
     }
+
 }
